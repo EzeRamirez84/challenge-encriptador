8b8407fa66cf4628d463152188c94036770257fc
ezeramirez84 <ezequielrmrz84@gmail.com>
2024-08-01 05:20:15 -0300

    agregue funcionalidad al boton de desencriptar y al boton de copiar con Clipboard API

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,97 @@
-
-let outputText = document.getElementById('output-text__textarea')
-
-let encriptarButton = document.getElementsByClassName('input-container__buttons__encriptar')[0]
+let encriptarButton = document.querySelector('.input-container__buttons__encriptar')
+let desencriptarButton = document.querySelector('.input-container__buttons__desencriptar')
 
 const handleEncriptarButton = () => {
   let text = document.getElementById('text')
-  console.log('text:',text);
-  let newText = encriptar(text.value)
-  let outputText = document.getElementById('output-text__textarea')
-  console.log('newText:', newText);
-  outputText.innerText = newText
+  if (text.value !== ''){
+    let newText = encriptar(text.value)
+    let outputText = document.getElementById('output-text__textarea')
+    outputText.innerText = newText
+    showOutput()
+  }else {
+    hideOutput()
+  }
 }
 encriptarButton.onclick = handleEncriptarButton
 console.log(encriptarButton);
 
+const handleDesencriptarButton = () => {
+  let text = document.getElementById('text')
+  if (text.value !== ''){
+    let newText = desencriptar(text.value)
+    let outputText = document.getElementById('output-text__textarea')
+    outputText.innerText = newText
+    showOutput()
+  }else {
+    hideOutput()
+  }
+}
+desencriptarButton.onclick = handleDesencriptarButton
 
-const encriptacion ={
+
+function showOutput() {
+
+  let noOutputView = document.querySelector('.no-output')
+  noOutputView.style.visibility = "hidden"
+  noOutputView.style.display = "none"
+
+  let outputTextView = document.querySelector('.output-text')
+  outputTextView.style.visibility = "visible"
+  outputTextView.style.display = "flex"
+}
+
+function hideOutput() {
+
+  let noOutputView = document.querySelector('.no-output')
+  noOutputView.style.visibility = "visible"
+  noOutputView.style.display = "flex"
+
+  let outputTextView = document.querySelector('.output-text')
+  outputTextView.style.visibility = "hidden"
+  outputTextView.style.display = "none"
+}
+
+const encriptacion = {
   'a': 'ai',
-  'e' : 'enter',
-  'i' : 'imes',
-  'o' : 'ober',
-  'u' : 'ufat'
+  'e': 'enter',
+  'i': 'imes',
+  'o': 'ober',
+  'u': 'ufat'
 }
 
 const desencriptacion = {
-  'ai':'a' ,
-  'enter' : 'e',
-  'imes' : 'i',
-  'ober' : 'o',
-  'ufat' : 'u'
+  'ai': 'a',
+  'enter': 'e',
+  'imes': 'i',
+  'ober': 'o',
+  'ufat': 'u'
 }
 
 function encriptar(texto) {
-  texto = texto.replace(/a|e|i|o|u/g, function(matched){
+  texto = texto.replace(/a|e|i|o|u/g, function (matched) {
     return encriptacion[matched];
   });
   return texto
 }
 
 function desencriptar(texto) {
-  texto = texto.replace(/ai|enter|imes|ober|ufat/g, function(matched){
+  texto = texto.replace(/ai|enter|imes|ober|ufat/g, function (matched) {
     return desencriptacion[matched];
   });
   return texto
 }
+
+//funcionalidad copiar con boton
+let copyButton = document.getElementById('copy')
+copyButton.addEventListener("click",() => writeClipboardText())
+
+async function writeClipboardText() {
+  try {
+    let text = document.getElementById('output-text__textarea').value
+    await navigator.clipboard.writeText(text);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+window.onclose(()=>copyButton.removeEventListener("click"))
